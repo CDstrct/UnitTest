72fd77a3795c4584699c5b0668034dea69418b18
CDstrct <kacper.mitrega@edu.uekat.pl>
2024-12-23 18:05:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="test_program",
+    name="test_program2137abcd",
     version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.7",
