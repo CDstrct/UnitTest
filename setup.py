31d8788e33f7a0ea983408501b22248a9bae4bbb
CDstrct <kacper.mitrega@edu.uekat.pl>
2024-12-23 17:52:26 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_program",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    description="abcd",
+    
+)
